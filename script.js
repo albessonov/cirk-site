a15d99c1118e223314d0d27d17a233d489ac0a99
Alexander Bessonov <sashabessonov@icloud.com>
2025-03-28 19:12:55 +0300

    added footer

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ document.querySelectorAll('.custom-dropdown').forEach(dropdown => {
         }
     });
 });
+document.getElementById('contact-form').addEventListener('submit', function(event) {  
+    event.preventDefault();  
+    var name = document.getElementById('name').value;  
+    var email = document.getElementById('email').value;  
+    var message = document.getElementById('message').value;  
+    var subject = 'Новое Сообщение от ' + name;  
+    var body = 'Email: ' + email + '\n\n' + message;  
+    var mailtoLink = 'mailto:sashabessonov@icloud.com?subject=' + encodeURI(subject) + '&body=' + encodeURI(body);  
+    window.location.href = mailtoLink;  
+});
